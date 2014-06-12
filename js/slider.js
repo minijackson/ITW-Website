37af72dd998a0f010e50ff8cf89785dd055d5841
Minijackson <minijackson@riseup.net>
2014-06-13 01:16:59 +0200

    Adding multi img-slider with captions/links

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
 	window.sliderFrame = 0;
+	window.productsSliderFrame = 0;
 
-	$(".slider-control-next").click(function() {
+	$(".image-slider .slider-control-next").click(function() {
 		nextSlide();
 		return false;
 	});
 
-	$(".slider-control-prev").click(function() {
+	$(".image-slider .slider-control-prev").click(function() {
 		--window.sliderFrame;
 		window.sliderFrame = (window.sliderFrame < 0) ?
 			window.sliderFrame + 4 : window.sliderFrame;
 		changeSliderFrame();
+		return false;
+	});
+
+	$(".products-slider .slider-control-next").click(function() {
+		++window.productsSliderFrame;
+		window.productsSliderFrame %= 5;
+		changeProductsSliderFrame();
+		return false;
+	});
+
+	$(".products-slider .slider-control-prev").click(function() {
+		--window.productsSliderFrame;
+		window.productsSliderFrame = (window.productsSliderFrame < 0) ?
+			window.productsSliderFrame + 5 : window.productsSliderFrame;
+		changeProductsSliderFrame();
 		return false;
 	});
 
@@ -40,6 +56,13 @@ $(document).ready(function() {
 		updateDots();
 		window.clearTimeout(slideTimeout);
 		slideTimeout = window.setTimeout(nextSlide, 5000);
+	}
+
+	function changeProductsSliderFrame() {
+		// transform: translateX(-i*5%);
+		$(".products-slider>ul").css({
+			"transform": "translate3d(-" + window.productsSliderFrame * 5 + "%, 0, 0)"
+		});
 	}
 
 	var slideTimeout = window.setTimeout(nextSlide, 5000);
